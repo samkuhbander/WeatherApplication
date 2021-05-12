2a23be7eba5f266b1a4f6483914cbb0b2f8af321
root <root@ip-172-31-30-42.ec2.internal>
2021-05-12 00:11:55 +0000

    Moved weather icons

--- final/script.js
+++ final/script.js
@@ -1,3 +1,5 @@
+var lat;
+var lon;
 
 	function zipRetrieve(zip){
 		a=$.ajax({
@@ -14,3 +16,47 @@
 				console.log(error);
 			});
 		}
+	function zipForecast(zip) {
+		a=$.ajax({
+                                url: 'https://api.clearllc.com/api/v2/miamidb/_table/zipcode/?api_key=bed859b37ac6f1dd59387829a18db84c22ac99c09ee0f5fb99cb708364858818&ids='+zip, method: "GET"
+                        }).done(function(data) {
+				lat = data.resource[0].latitude;
+				lon = data.resource[0].longitude;
+				console.log(lat);
+				console.log(lon);
+				
+				a=$.ajax({
+					url: "https://api.openweathermap.org/data/2.5/onecall",
+					method: "GET",
+				data: {
+					"lat": lat,
+					"lon": lon,
+					"exclude": "hourly",
+					"appid": "defe9caad8afe38ba65d7cee31b8515a" 
+				}
+				}).done(function(data) {
+					console.log(data);
+					$("#day1").append("Weather: " + data.daily[0].weather[0].main + "<br>");
+					$("#day1").append("Low: " + data.daily[0].temp.min + "<br>");
+					$("#day1").append("High: " + data.daily[0].temp.max  + "<br>");
+					$("#day1").append(getDay(0));
+				}).fail(function(error) {
+                                	console.log(error);
+                        	});
+
+                        }).fail(function(error) {
+                                console.log(error);
+                        });
+	}
+
+	function getDay(numb){
+		var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+		var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul","Aug", "Sep", "Oct", "Nov", "Dec"];
+                                var d = new Date()+numb;
+				var name = days[d.getDay()];
+				var month = months[d.getMonth()];
+				var day = d.getDate();
+				var output = name + " " + month + " " + day;
+				return output;
+	}
+
